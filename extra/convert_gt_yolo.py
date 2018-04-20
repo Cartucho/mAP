f338d72ae9437a07320791c1f58901ba8b8f0e53
cartucho <joaocartuchoo@gmail.com>
2018-04-20 15:57:50 +0100

    move YOLO conversion to function

--- extra/convert_gt_yolo.py
+++ extra/convert_gt_yolo.py
@@ -4,6 +4,23 @@ import glob
 import cv2
 
 
+def convert_yolo_coordinates_to_voc(x_c_n, y_c_n, width_n, height_n, img_width, img_height):
+  ## remove normalization given the size of the image
+  x_c = float(x_c_n) * img_width
+  y_c = float(y_c_n) * img_height
+  width = float(width_n) * img_width
+  height = float(height_n) * img_height
+  ## compute half width and half height
+  half_width = width / 2
+  half_height = height / 2
+  ## compute left, top, right, bottom
+  ## in the official VOC challenge the top-left pixel in the image has coordinates (1;1)
+  left = int(x_c - half_width) + 1
+  top = int(y_c - half_height) + 1
+  right = int(x_c + half_width) + 1
+  bottom = int(y_c + half_height) + 1
+  return left, top, right, bottom
+
 # read the class_list.txt to a list
 with open("class_list.txt") as f:
   obj_list = f.readlines()
@@ -60,20 +77,7 @@ for tmp_file in txt_list:
       ## "c" stands for center and "n" stands for normalized
       obj_id, x_c_n, y_c_n, width_n, height_n = line.split()
       obj_name = obj_list[int(obj_id)]
-      ## remove normalization given the size of the image
-      x_c = float(x_c_n) * img_width
-      y_c = float(y_c_n) * img_height
-      width = float(width_n) * img_width
-      height = float(height_n) * img_height
-      ## compute half width and half height
-      half_width = width / 2
-      half_height = height / 2
-      ## compute left, top, right, bottom
-      ## in the official VOC challenge the top-left pixel in the image has coordinates (1;1)
-      left = int(x_c - half_width) + 1
-      top = int(y_c - half_height) + 1
-      right = int(x_c + half_width) + 1
-      bottom = int(y_c + half_height) + 1
+      left, top, right, bottom = convert_yolo_coordinates_to_voc(x_c_n, y_c_n, width_n, height_n, img_width, img_height)
       ## add new line to file
       #print(obj_name + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(bottom))
       new_f.write(obj_name + " " + str(left) + " " + str(top) + " " + str(right) + " " + str(bottom) + '\n')
