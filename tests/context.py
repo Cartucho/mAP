720e6d1cbfe3f413d7141aad3a35653c8133d656
Peter Siemen <peter.siemen@googlemail.com>
2019-11-25 13:48:59 +0100

    mv src to lib

--- tests/context.py
+++ tests/context.py
@@ -3,14 +3,14 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib/mAP')))
 
 from mAP.bounding_box import *
 from mAP.detection import *
 from mAP.ground_truth import *
 from mAP.classify import *
 from mAP.iou import *
-from mean_average_precision import MeanAveragePrecision
+from mAP.mean_average_precision import MeanAveragePrecision
 from mAP.precision import Precision
 from mAP.recall import Recall
 from mAP.average_precision import *
