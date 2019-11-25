2e2d7c3969edc3c872e58664bc6ec2f481af600d
Peter Siemen <peter.siemen@googlemail.com>
2019-11-25 13:38:58 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/petersiemen/mAP",
     packages=setuptools.find_packages(),
+    package_dir = {'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
